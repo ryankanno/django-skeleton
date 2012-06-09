68bfae65e8a394d0cb33d2fcae83bcb984982c93
Ryan Kanno <ryankanno@localkinegrinds.com>
2012-06-09 10:56:02 -0400

    Added staticfiles to settings.DEBUG

--- sample_project/urls.py
+++ sample_project/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,3 +11,6 @@ urlpatterns = patterns('',
      url(r'^$', www_views.slash, name="slash"),
      url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
